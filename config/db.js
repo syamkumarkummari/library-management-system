a7ec686babe51798b0f627c7ecdd1616bb03b805
SyamKumar <syamkumar81@email.com>
2026-04-10 14:30:39 +0530

    added ssl config

--- config/db.js
+++ config/db.js
@@ -6,14 +6,18 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Database Error:", err);
+    console.log("❌ Database Error:", err);
   } else {
-    console.log("MySQL Connected...");
+    console.log("✅ MySQL Connected...");
   }
 });
 
